6e6ff8af95a2722c0b74527e4e9d6f2f74581fd1
liangshaojiang <920999181@qq.com>
2021-01-09 14:59:46 +0800

    添加小程序检查更新

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script>
-		import Vue from 'vue'
+	import Vue from 'vue'
 	export default {
-		onLaunch: function() { 
+		onLaunch: function() {
 			// console.log('App Launch')
 			uni.getSystemInfo({
 				success: function(e) {
@@ -13,7 +13,7 @@
 						Vue.prototype.CustomBar = e.statusBarHeight + 45;
 					};
 					// #endif
-			
+
 					// #ifdef MP-WEIXIN || MP-QQ
 					Vue.prototype.StatusBar = e.statusBarHeight;
 					let capsule = wx.getMenuButtonBoundingClientRect();
@@ -25,14 +25,44 @@
 						Vue.prototype.CustomBar = e.statusBarHeight + 50;
 					}
 					// #endif		
-				
-			
+
+
 					// #ifdef MP-ALIPAY
 					Vue.prototype.StatusBar = e.statusBarHeight;
 					Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
 					// #endif
 				}
 			})
+
+			<!-- #ifdef MP-WEIXIN -->
+			//检查更新
+			const updateManager = wx.getUpdateManager()
+
+			updateManager.onCheckForUpdate(function(res) {
+				// 请求完新版本信息的回调
+				console.log(res.hasUpdate)
+			})
+
+			updateManager.onUpdateReady(function() {
+				uni.showModal({
+					title: '更新提示',
+					content: '新版本已经准备好，是否重启应用？',
+					success: function(res) {
+						if (res.confirm) {
+							updateManager.applyUpdate()
+						} else if (res.cancel) {
+							// console.log('用户点击取消');
+						}
+					}
+				});
+			})
+			updateManager.onUpdateFailed(function() {
+				// 新版本下载失败
+			})
+ 
+ 
+ 
+			<!-- #endif -->
 		},
 		onShow: function() {
 			// console.log('App Show')
@@ -47,12 +77,10 @@
 	@import "colorui/main.css";
 	@import "colorui/icon.css";
 	@import "colorui/iconfont/iconfont.css";
+
 	/*每个页面公共css */
-	.cu-item:active{
+	.cu-item:active {
 		background-color: #e5e5e5 !important;
 		transition: all .1s ease-in-out 0s;
 	}
-	
-	
-	
 </style>
